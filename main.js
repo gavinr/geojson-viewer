d76f32aaf13f6c8aa3a4eaa9c4c5e860f4aba86a
Gavin Rehkemper <gavin@gavinr.com>
2021-06-26 22:49:29 -0500

    remove jquery and update to latest ArcGIS JS API

--- main.js
+++ main.js
@@ -1,61 +1,59 @@
 function getUrlParameter() {
-	var sPageURL = window.location.search.substring(1);
-	if (sPageURL !== '') {
-		var location = sPageURL.indexOf("=");
-		return sPageURL.substring(location + 1);
-	} else {
-		return false;
-	}
+  var sPageURL = window.location.search.substring(1);
+  if (sPageURL !== "") {
+    var location = sPageURL.indexOf("=");
+    return sPageURL.substring(location + 1);
+  } else {
+    return false;
+  }
 }
 
 require([
-	"esri/Map",
-	"esri/views/MapView",
-	"esri/layers/GeoJSONLayer"
-], function(Map, MapView, GeoJSONLayer) {
+  "esri/Map",
+  "esri/views/MapView",
+  "esri/layers/GeoJSONLayer",
+], function (Map, MapView, GeoJSONLayer) {
+  var url = getUrlParameter("url");
+  if (url !== false) {
+    const element = document.querySelectorAll("#entryFormWrapper")[0];
+    element.parentNode.removeChild(element);
 
-	var url = getUrlParameter('url');
-	if (url !== false) {
-		$("#entryFormWrapper").remove();
+    var geojsonLayer = new GeoJSONLayer({
+      url: url,
+    });
 
-		var geojsonLayer = new GeoJSONLayer({
-			url: url
-		});
+    geojsonLayer.watch("loadStatus", function (status) {
+      if (status === "failed") {
+        alert("Error. Please check the URL and click 'Try Another'.");
+      }
+    });
 
-		geojsonLayer.watch('loadStatus', function(status) {
-			if(status === 'failed') {
-				alert("Error. Please check the URL and click 'Try Another'.");
-			}
-		});
+    geojsonLayer.when(function () {
+      view.extent = geojsonLayer.fullExtent;
+      geojsonLayer.popupTemplate = geojsonLayer.createPopupTemplate();
+    });
 
-		
-		geojsonLayer.when(function(){
-			view.extent = geojsonLayer.fullExtent;
-			geojsonLayer.popupTemplate = geojsonLayer.createPopupTemplate();
-		});
+    var map = new Map({
+      basemap: "gray",
+      layers: [geojsonLayer],
+    });
 
-		var map = new Map({
-			basemap: "gray",
-			layers: [geojsonLayer]
-		});
-	
-		var view = new MapView({
-			container: "viewDiv",
-			map: map,
-			center: [0,0],
-			zoom: 3
-		});
+    var view = new MapView({
+      container: "viewDiv",
+      map: map,
+      center: [0, 0],
+      zoom: 3,
+    });
+  } else {
+    const element = document.querySelectorAll("#map")[0];
+    element.parentNode.removeChild(element);
 
-		
-	} else {
-		$("#map").remove();
-
-		$("#lookupUrlButton").click(function(event) {
-			event.preventDefault();
-			var newUrl = window.location.href + '?url=' + $("#urlInput").val();
-			window.location.replace(newUrl);
-		});
-	}
-
+    document
+      .querySelectorAll("#lookupUrlButton")[0]
+      .addEventListener("click", function (event) {
+        event.preventDefault();
+        var newUrl = window.location.href + "?url=" + $("#urlInput").val();
+        window.location.replace(newUrl);
+      });
+  }
 });
-
