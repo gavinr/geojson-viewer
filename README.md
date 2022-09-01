327ee5deae804e1b818088233309eed313fb069e
Gavin Rehkemper <gavin@gavinr.com>
2022-09-01 00:07:40 -0500

    swipe and urlencode

--- README.md
+++ README.md
@@ -4,14 +4,21 @@ View GeoJSON based on a URL param
 
 ## Usage
 
-Pass a GeoJSON url to the parameter in the form https://gavinr.github.io/geojson-viewer/?url=________ 
+Pass a GeoJSON url to the parameter in the form <https://gavinr.github.io/geojson-viewer/?url=________>
 
 Examples:
 
-* [Demo 1 - US National Parks](https://gavinr.github.io/geojson-viewer/?url=http://services1.arcgis.com/g2TonOxuRkIqSOFx/arcgis/rest/services/US_National_Parks/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&distance=&units=esriSRUnit_Meter&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&quantizationParameters=&f=pgeojson&token=)
-* [Demo 2 - Washington DC Bike Lanes](https://gavinr.github.io/geojson-viewer/?url=https://cdn.jsdelivr.net/gh/Esri/geojson-layer-js@master/data/dc-bike-lanes.json)
-* [Demo 3 - Allegheny County Blazed Trails Locations](https://gavinr.github.io/geojson-viewer/?url=https://opendata.arcgis.com/datasets/d182439a9a6344fca2c5bf717b9cace8_0.geojson)
+* [Demo 1 - US National Parks](https://gavinr.github.io/geojson-viewer/?url=http%3A%2F%2Fservices1.arcgis.com%2Fg2TonOxuRkIqSOFx%2Farcgis%2Frest%2Fservices%2FUS_National_Parks%2FFeatureServer%2F0%2Fquery%3Fwhere%3D1%253D1%26objectIds%3D%26time%3D%26geometry%3D%26geometryType%3DesriGeometryEnvelope%26inSR%3D%26spatialRel%3DesriSpatialRelIntersects%26distance%3D%26units%3DesriSRUnit_Meter%26outFields%3D*%26returnGeometry%3Dtrue%26maxAllowableOffset%3D%26geometryPrecision%3D%26outSR%3D4326%26returnIdsOnly%3Dfalse%26returnCountOnly%3Dfalse%26returnExtentOnly%3Dfalse%26orderByFields%3D%26groupByFieldsForStatistics%3D%26outStatistics%3D%26resultOffset%3D%26resultRecordCount%3D%26returnZ%3Dfalse%26returnM%3Dfalse%26quantizationParameters%3D%26f%3Dpgeojson%26token%3D)
+* [Demo 2 - Washington DC Bike Lanes](https://gavinr.github.io/geojson-viewer/?url=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FEsri%2Fgeojson-layer-js%40master%2Fdata%2Fdc-bike-lanes.json)
+* [Demo 3 - Allegheny County Blazed Trails Locations](https://gavinr.github.io/geojson-viewer/?url=https%3A%2F%2Fopendata.arcgis.com%2Fdatasets%2Fd182439a9a6344fca2c5bf717b9cace8_0.geojson)
 
 ## Technologies
 
 * [ArcGIS API for JavaScript](https://developers.arcgis.com/javascript/) - [GeoJSONLayer](https://developers.arcgis.com/javascript/latest/api-reference/esri-layers-GeoJSONLayer.html#)
+
+## Advanced Usage
+
+Some additional URL parameters you can use:
+
+* `silent=true` - do not show the buttons or GitHub banner. [Example](https://gavinr.github.io/geojson-viewer/?silent=true&url=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FEsri%2Fgeojson-layer-js%40master%2Fdata%2Fdc-bike-lanes.json)
+* `swipeLayer=<url>` - second layer to show as a swipe. [Example](https://gavinr.github.io/geojson-viewer/?url=https%3A%2F%2Fservices.arcgis.com%2FP3ePLMYs2RVChkJx%2Farcgis%2Frest%2Fservices%2FUSA_States_Generalized%2FFeatureServer%2F0%2Fquery%3Fwhere%3D1%253D1%26outFields%3D*%26f%3Dgeojson&swipeLayer=https%3A%2F%2Fservices.arcgis.com%2FP3ePLMYs2RVChkJx%2Farcgis%2Frest%2Fservices%2FUSA_Counties_Generalized%2FFeatureServer%2F0%2Fquery%3Fwhere%3DPOP_SQMI%3E50%26outFields%3D*%26f%3Dgeojson)
--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-function getUrlParameter() {
+function getUrlParameter(c) {
+  const url = new URL(window.location);
+  return url.searchParams.get(c);
+}
+
+function getUrlParameterOld() {
   var sPageURL = window.location.search.substring(1);
   if (sPageURL !== "") {
     var location = sPageURL.indexOf("=");
@@ -12,22 +17,13 @@ require([
   "esri/Map",
   "esri/views/MapView",
   "esri/layers/GeoJSONLayer",
-], function (Map, MapView, GeoJSONLayer) {
+  "esri/widgets/Swipe",
+], function (Map, MapView, GeoJSONLayer, Swipe) {
   var url = getUrlParameter("url");
-  if (url !== false) {
-    const element = document.querySelectorAll("#entryFormWrapper")[0];
-    element.parentNode.removeChild(element);
+  var silent = getUrlParameter("silent");
+  var swipeLayerParam = getUrlParameter("swipeLayer");
 
-    var geojsonLayer = new GeoJSONLayer({
-      url: url,
-    });
-
-    geojsonLayer.watch("loadStatus", function (status) {
-      if (status === "failed") {
-        alert("Error. Please check the URL and click 'Try Another'.");
-      }
-    });
-
+  const finishLoadingMap = (geojsonLayer, swipeLayerUrl) => {
     geojsonLayer.when(function () {
       view.extent = geojsonLayer.fullExtent;
       geojsonLayer.popupTemplate = geojsonLayer.createPopupTemplate();
@@ -44,6 +40,66 @@ require([
       center: [0, 0],
       zoom: 3,
     });
+
+    if (swipeLayerUrl) {
+      const swipeLayer = new GeoJSONLayer({
+        url: swipeLayerUrl,
+      });
+      map.add(swipeLayer);
+      let swipe = new Swipe({
+        view: view,
+        leadingLayers: [geojsonLayer],
+        trailingLayers: [geojsonLayer, swipeLayer],
+        // direction: "vertical", // swipe widget will move from top to bottom of view
+        position: 50, // position set to middle of the view (50%)
+      });
+      view.ui.add(swipe);
+    }
+  };
+
+  const tryToMakeLayer = (url) => {
+    return new Promise((resolve, reject) => {
+      const geojsonLayer = new GeoJSONLayer({
+        url: url,
+      });
+
+      geojsonLayer.watch("loadStatus", function (status) {
+        if (status === "failed") {
+          reject();
+        } else if (status === "loaded") {
+          resolve(geojsonLayer);
+        }
+      });
+
+      geojsonLayer.load();
+    });
+  };
+
+  if (url !== false && url !== null) {
+    const element = document.querySelectorAll("#entryFormWrapper")[0];
+    element.parentNode.removeChild(element);
+
+    tryToMakeLayer(url).then(
+      (layer) => {
+        finishLoadingMap(layer, swipeLayerParam);
+      },
+      () => {
+        // error - could not load
+        const url2 = getUrlParameterOld();
+        tryToMakeLayer(url2).then(
+          (layer) => {
+            console.warn(
+              "PLEASE URL ENCODE YOUR URLS - THIS URL FORMAT WILL STOP WORKING SOON - https://meyerweb.com/eric/tools/dencoder/"
+            );
+            finishLoadingMap(layer, swipeLayerParam);
+          },
+          () => {
+            // error after try 2
+            alert("Error. Please check the URL and click 'Try Another'.");
+          }
+        );
+      }
+    );
   } else {
     const element = document.querySelectorAll("#map")[0];
     element.parentNode.removeChild(element);
@@ -53,8 +109,16 @@ require([
       .querySelectorAll("#lookupUrlButton")[0]
       .addEventListener("click", function (event) {
         event.preventDefault();
-        var newUrl = window.location.href + "?url=" + urlInput.value;
+        var newUrl =
+          window.location.href + "?url=" + encodeURIComponent(urlInput.value);
         window.location.replace(newUrl);
       });
+  }
+
+  if (silent === "true") {
+    const idsToHide = ["githubFlagLink", "buttonWrapper"];
+    idsToHide.forEach((id) => {
+      document.getElementById(id).classList.add("hidden");
+    });
   }
 });
